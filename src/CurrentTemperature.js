3d0ab68bf4b71437c0d0e13ec6b60b64f615144b
Anhelina Kruptseva <109616834+Axigy@users.noreply.github.com>
2022-11-01 19:13:25 +0100

    Update CurrentTemperature.js

--- src/CurrentTemperature.js
+++ src/CurrentTemperature.js
@@ -4,7 +4,7 @@ export default function CurrentTemperature(prop) {
   return (
     <h5 className="currentTemp mt-1">
       <span id="temp-value">{Math.round(prop.celsiusTemp)}</span>
-      <span className="degreesIcon">°C | °F</span>
+      <span className="degreesIcon">°C</span>
     </h5>
   );
 }
