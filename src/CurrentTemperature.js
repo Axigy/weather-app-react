7eb343f9485d763739243a3e3dcc2e9c8dfe9c34
Anhelina Kruptseva <109616834+Axigy@users.noreply.github.com>
2022-11-01 19:00:08 +0100

    delete fahrenhait

--- src/CurrentTemperature.js
+++ src/CurrentTemperature.js
@@ -1,40 +1,10 @@
 import React, { useState } from "react";
 
 export default function CurrentTemperature(prop) {
-  let [unit, setUnit] = useState("celsius");
-  function showFahrenheit(e) {
-    e.preventDefault();
-    setUnit("fahrenheit");
-  }
-  function showCelsius(e) {
-    e.preventDefault();
-    setUnit("celsius");
-  }
-  if (unit === "celsius") {
-    return (
-      <h5 className="currentTemp mt-1">
-        <span id="temp-value">{Math.round(prop.celsiusTemp)}</span>
-        <span className="degreesIcon">
-          °C |
-          <a href="/" onClick={showFahrenheit}>
-            {" "}
-            °F
-          </a>
-        </span>
-      </h5>
-    );
-  } else {
-    let fahrenheit = (prop.celsiusTemp * 9) / 5 + 32;
-    return (
-      <h5 className="currentTemp mt-1">
-        <span id="temp-value">{Math.round(fahrenheit)}</span>
-        <span className="degreesIcon">
-          <a href="/" onClick={showCelsius}>
-            °C
-          </a>{" "}
-          | °F
-        </span>
-      </h5>
-    );
-  }
+  return (
+    <h5 className="currentTemp mt-1">
+      <span id="temp-value">{Math.round(prop.celsiusTemp)}</span>
+      <span className="degreesIcon">°C | °F</span>
+    </h5>
+  );
 }
